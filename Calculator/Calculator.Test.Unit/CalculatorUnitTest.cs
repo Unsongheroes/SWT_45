374d1b31d2b5d04a14dc0a9005ae462e56fe30ea
Kristoffer Stampe Villadsen <Villadsen67@hotmail.com>
2018-02-01 11:36:17 +0100

    flere tests

---
+++ Calculator/Calculator.Test.Unit/CalculatorUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using SimpleCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Test.Unit
+{
+    [TestFixture]
+    public class CalculatorUnitTest
+    {
+        [Test]
+        public void Add_Add2And4_results6()
+        {
+            var uut = new SimpleCalculator1();
+
+            Assert.That(uut.Add(2, 4), Is.EqualTo(6));
+        }
+
+        [TestCase(2, 3,5)]
+        [TestCase(2, -3,-1)]
+        [TestCase(-2, -3, -5)]
+        [TestCase(0, 0, 0)]
+        public void Add_Testcases_VaryResult(int a, int b, int d)
+        {
+            var uut = new SimpleCalculator1();
+            Assert.AreEqual(d,uut.Add(a,b));
+        }
+
+    }
+}
